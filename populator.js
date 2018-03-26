e9f6d9610f83978e889a6e9673840a4c09016fb4
otboss <otsurfer6@gmail.com>
2018-03-26 06:05:35 -0500

    updated populator.js

--- populator.js
+++ populator.js
@@ -28,21 +28,22 @@ generateRecords = function(accountTable, recipeTable, numberOfAccounts, numberOf
         accountSql = "INSERT INTO "+accountTable+" (id, email, password, salt, fname, lname, dob, diet) VALUES"; 
         name = "";
         salt = "";
-        for(i=0; i < numberOfAccounts; i++) {
+        for(var i=0; i < numberOfAccounts; i++) {
             name = faker.name.firstName()+" "+faker.name.lastName();
+            name = name.replace("'","")
             salt = sha256(faker.name.firstName()).slice(0, 10);
             accountSql += "\n("+i+", ";
-            accountSql += "'"+name.split(" ")[0]+"_"+name.split(" ")[1]+"@"+emails[Math.floor((Math.random() * 4) + 0)]+"', ";
+            accountSql += "'"+name.split(" ")[0]+"_"+name.split(" ").slice(1, name.length)+"@"+emails[Math.floor((Math.random() * 4) + 0)]+"', ";
             accountSql += "'"+sha256(faker.address.city())+"',"
             accountSql += "'"+salt+"', ";
             accountSql += "'"+name.split(" ")[0]+"',";
-            accountSql += "'"+name.split(" ")[1]+"',";
+            accountSql += "'"+name.split(" ").slice(1, name.length)+"',";
             accountSql += "'"+formatDate(faker.date.between('1960-09-31','2018-02-28'))+"',";
             accountSql += "'"+diets[Math.floor((Math.random() * 3) + 0)]+"')";
             if(i == numberOfAccounts - 1)
-                accountSql += ",";
-            else
                 accountSql += ";";
+            else
+                accountSql += ",";
         }
         resolve(accountSql)
     });
